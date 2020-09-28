e8162e3d78120dc6919a8193068ed90fd13e278c
Kalsang Agongtsang <tznklsng@yahoo.com>
2020-09-28 19:10:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function takeANumber( katzDeli,name ){
-  katzDeli.push(`${name}`);
-  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+function takeANumber( katzDeli){
+  var num = 0;
+  num++;
+  katzDeli.push(num)
+  return (`Welcome you are ticket number ${num} in line.`);
+  
 }
 
 function nowServing(katzDeli){
